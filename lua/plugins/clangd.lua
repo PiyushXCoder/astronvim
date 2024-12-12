e2f0cefc69c71aed0dd09d9906b1435c170804c0
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-12-12 19:09:02 +0530

    feat: support custom compiler in clangd
    It is helpful specially in cross compilation.

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -1,19 +1,44 @@
-return {
+return  {
   {
     "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-      config = {
-        clangd = {
-          capabilities = {
-            offsetEncoding = "utf-8",
+--    ---@type AstroLSPOpts
+    opts = function(plugin, opts)
+      local f = io.open('.nvim_lspconfig.lua')
+      local query_driver= nil
+      if f ~= nil then
+        io.close(f)
+        local conf = dofile('.nvim_lspconfig.lua')
+        query_driver = "--query-driver=" .. conf.query_driver
+      end
+      -- vim.notify("=> " .. query_driver, "info")
+
+      local cmd = {
+        "clangd",
+        "--enable-config",
+        "--clang-tidy",
+        "--all-scopes-completion=false",
+        "--background-index",
+        "--header-insertion=never",
+        "--pch-storage=memory",
+      };
+
+      if query_driver ~= nil then
+        table.insert(cmd, query_driver)
+      end
+
+
+
+      return {
+        config = {
+          clangd = {
+            capabilities = {
+              offsetEncoding = "utf-8",
+            },
+            cmd = cmd
           },
-          cmd = {
-            "clangd", "-header-insertion=never"
-          }
         },
-      },
-    },
+      }
+    end,
   },
   {
     "p00f/clangd_extensions.nvim", -- install lsp plugin
@@ -21,7 +46,7 @@ return {
     init = function()
       -- load clangd extensions when clangd attaches
       local augroup =
-        vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
+      vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
       vim.api.nvim_create_autocmd("LspAttach", {
         group = augroup,
         desc = "Load clangd_extensions with clangd",
@@ -31,17 +56,17 @@ return {
             == "clangd"
           then
             require("clangd_extensions")
-            -- add more `clangd` setup here as needed such as loading autocmds
-            vim.api.nvim_del_augroup_by_id(augroup) -- delete auto command since it only needs to happen once
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "clangd" }, -- automatically install lsp
+              -- add more `clangd` setup here as needed such as loading autocmds
+              vim.api.nvim_del_augroup_by_id(augroup) -- delete auto command since it only needs to happen once
+            end
+          end,
+        })
+      end,
     },
-  },
-}
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "clangd" }, -- automatically install lsp
+      },
+    },
+  }
