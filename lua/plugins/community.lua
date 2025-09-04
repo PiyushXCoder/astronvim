9083b0f7e6049777f3c131afb664cd0ad0bac9e9
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-09-04 20:12:55 +0530

    refactor: add typescript community

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -1,5 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
 }
