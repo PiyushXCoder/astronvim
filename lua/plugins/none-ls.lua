cd973cd985abade5c45923a9c0c1d06e580ded7f
PiyushXCoder <piyushxcoder@gmail.com>
2025-11-04 17:19:28 +0530

    refactor: disable eslint if not available

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,6 +2,18 @@
 
 -- Customize None-ls sources
 
+local function has_eslint_config(utils)
+  return utils.root_has_file({
+    ".eslintrc",
+    ".eslintrc.cjs",
+    ".eslintrc.js",
+    ".eslintrc.json",
+    "eslint.config.cjs",
+    "eslint.config.js",
+    "eslint.config.mjs",
+  })
+end
+
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
@@ -20,7 +32,7 @@ return {
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       -- Set a formatter
-      require("none-ls.diagnostics.eslint_d"),        
+      require("none-ls.diagnostics.eslint_d").with({ condition = has_eslint_config }),        
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
     })
