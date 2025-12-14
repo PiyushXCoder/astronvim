0444a6c0f9d613404c47e465ea8cb5d3d5a6c096
PiyushXCoder <piyushxcoder@gmail.com>
2025-12-14 19:19:26 +0530

    refactor: imporved rust lsp

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,24 +1,55 @@
 return {
   {
     "AstroNvim/astrolsp",
---    ---@type AstroLSPOpts
-    opts = function(plugin, opts)
-
-      local cmd = {
-        "systemd-run","--scope","-p", "MemoryMax=6144M", "-p", "MemoryHigh=6144M",
-        "--user",
-        "rust-analyzer"
-        -- "/home/piyush/.cargo/bin/ra-multiplex",
-      };
-
-
-      -- extend our configuration table to have our new prolog server
+    opts = function(_, opts)
       opts.config = require("astrocore").extend_tbl(opts.config or {}, {
-          rust_analyzer = {
-            cmd = cmd
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allTargets = true,
+              },
+            },
           },
-        })
-
+        },
+      })
     end,
-  }
+  },
 }
+
+
+-- The belowe is an alternative standalone configuration for rust-analyzer. You can
+-- learn how differtent LSPs are configured in AstroNvim:
+--
+-- AstroNvim Configuration:
+--
+-- return {
+--   {
+--     "AstroNvim/astrolsp",
+--     opts = function(_, opts)
+--       opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+--         rust_analyzer = {
+--           settings = {
+--             ["rust-analyzer"] = {
+--               cargo = {
+--                 allTargets = true,
+--               },
+--             },
+--           },
+--         },
+--       })
+--     end,
+--   },
+-- }
+--
+-- Standalone Configuration:
+--
+-- require("lspconfig").rust_analyzer.setup({
+--   settings = {
+--     ["rust-analyzer"] = {
+--       cargo = {
+--         allTargets = true,
+--       },
+--     },
+--   },
+-- })
