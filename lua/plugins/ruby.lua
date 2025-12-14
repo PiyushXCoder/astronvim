fdc85bae39b9c09090a4be4650cca2551b576f43
PiyushXCoder <piyushxcoder@gmail.com>
2025-12-14 19:02:16 +0530

    feat: ruby support

--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -16,12 +16,9 @@ return {
         and { "bundle", "exec", "ruby-lsp" }
         or fallback
 
-      vim.notify("Ruby = " .. tostring(installed), vim.log.levels.INFO)
-      vim.notify(vim.inspect(cmd), vim.log.levels.INFO)
-
       opts.config = require("astrocore").extend_tbl(opts.config or {}, {
         ruby = {
-          cmd = cmd
+          -- cmd = cmd
         },
       })
     end,
