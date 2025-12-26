35855191e050165627025afdec114f2a25b3adf6
Piyush Raj Mishra <piyushxcoder@gmail.com>
2025-12-26 18:29:32 +0530

    feat: treesitter for grub

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,23 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+  opts = function (_, opts)
+    vim.filetype.add({
+      pattern = {
+        [".*grub.cfg"] = "grub",
+      },
+    })
+    vim.treesitter.language.register("bash", "grub")
+
+    opts.ensure_installed = opts.ensure_installed or {}
+    opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
+      ensure_installed = {
+        "lua",
+        "vim",
+        -- add more arguments for adding more treesitter parsers
+      },
+    });
+  end,
 }
