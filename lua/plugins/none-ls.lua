9489846f705caa68a57fe4ffee7f1d7c4f9d4c9b
Piyush Raj Mishra <piyushxcoder@gmail.com>
2026-01-31 21:31:20 +0530

    feat: eslint linter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -22,7 +22,6 @@ return {
   },
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -32,7 +31,7 @@ return {
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       -- Set a formatter
-      require("none-ls.diagnostics.eslint_d").with({ condition = has_eslint_config }),        
+      require("none-ls.diagnostics.eslint_d").with({ condition = has_eslint_config }),
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
     })
